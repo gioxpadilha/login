354456ade6495c9be5e1f895f06ce3ee03399001
gioxpadilha <57300930+gioxpadilha@users.noreply.github.com>
2021-01-08 16:31:27 -0300

    Add files via upload

---
+++ login-site-noticia-master/config.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+	define("HOME_URI", "http://gioxpadilha-com.umbler.net///");
+	define("HOME_DIR", "");
+
+	/*Parâmetros de conexão com o banco de dados*/
+	define('SGBD','mysql');
+	define('HOST_DB', 'mysql742.umbler.com');
+	define('DB', 'usuariogiovana');
+	define('USER_DB', 'gioxpadilha');
+	define('PASS_DB', 'testehospedagem');
+
+	define("DEFAULT_PASS", "usuario");
+?>
+
+
+
+
+
